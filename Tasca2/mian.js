2051834d99671db6e98a05fb78e22c2a9dbe9c68
idrojone <113390120+idrojone22@users.noreply.github.com>
2023-02-22 22:27:23 +0100

    Delete mian.js

--- Tasca2/mian.js
+++
@@ -1,9 +0,0 @@
-function nom() {
-    let nomUsuari = prompt('nombre de usuario')
-    
-    if (nomUsuari == Toni || toni) {
-      pantalla.innerHTML = `${nomUsuari}, eres un CRACK!`
-    } else {
-      pantalla.innerHTML = `Hola ${nomUsuari}, em sap mal, però Toni és més crack que tu...`
-    }
-}
