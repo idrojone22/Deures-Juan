0ddc4a77b7e76c6a8276fece8ee550c916e27820
jordi <jordivallsplasmx@gmail.com>
2023-02-22 22:26:42 +0100

    Tasca 2

--- Tasca2/main.js
+++ Tasca2/main.js
@@ -1,7 +1,7 @@
 function nom() {
     let nomUsuari = prompt('nombre de usuario')
     
-    if (nomUsuari == 'Toni' || 'toni') {
+    if (nomUsuari == 'Toni') {
       pantalla.innerHTML = `${nomUsuari}, eres un CRACK!`
     } else {
       pantalla.innerHTML = `Hola ${nomUsuari}, em sap mal, però Toni és més crack que tu...`
