ab8690bcfdff25e85e8648addc1f9af7dc7bd1ad
Jordi <jordivallsplasmx@gmail.com>
2023-02-28 21:58:22 +0100

    Aço ja arranca

--- Tasca5/main.js
+++ Tasca5/main.js
@@ -6,8 +6,8 @@ function numeroCartes() {
     for (let i = 1; i <= n; i++) {
         tauler.innerHTML += `<p> Carta ${i} </p>`
     }
+    
     let p = document.querySelectorAll('p')
-
     p.forEach(e => {
         let prova = () => {
             e.style.background = 'lightgreen'
