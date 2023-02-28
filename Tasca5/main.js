481f81572069ef2867e551a4ee51fde1a1ffb93a
Jordi <jordivallsplasmx@gmail.com>
2023-02-28 20:48:24 +0100

    Juan esta me la he currat

---
+++ Tasca5/main.js
@@ -0,0 +1,21 @@
+function numeroCartes() {
+    let n = prompt('Give me a number')
+    let contador = 0
+    tauler.innerHTML = ''
+
+    for (let i = 1; i <= n; i++) {
+        tauler.innerHTML += `<p> Carta ${i} </p>`
+    }
+    let p = document.querySelectorAll('p')
+
+    p.forEach(e => {
+        let prova = () => {
+            e.style.background = 'lightgreen'
+            e.innerHTML = 'P'
+            contador++
+            contador == n ?  setTimeout(() => {alert('Frena Moreno!')}, 200) : null
+            e.removeEventListener('click', prova)
+        }
+        e.addEventListener('click', prova)
+    })
+}
