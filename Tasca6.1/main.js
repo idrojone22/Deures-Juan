52f25228aa22d97a27e4a96051c9870d64a79d67
Jordi <jordivallsplasmx@gmail.com>
2023-03-07 20:17:55 +0100

    ·

--- Tasca6.1/main.js
+++ Tasca6.1/main.js
@@ -1,25 +1,29 @@
-contador = 0
-primeraCarta;
-segonaCarta;
+let contador = 0;
+let primeraCarta;
+let segonaCarta;
 function numeroCartes() {
     cartes = [1,2,3,3,2,1]
-    let contador = 0;
     cartes.forEach(e => { 
         tauler.innerHTML += `<p onclick="p(this)"> Carta ${e} </p>`;
     });  
 };
 function p(e){
     if (contador == 0) {
-        primeraCarta = e.id;
+        primeraCarta = e.textContent;
+        alert(`Has apretat la carta ${e.textContent}`);
         contador++;
-    } else if (contador == 1){
-        segonaCarta = e.id;
+    } else if (contador == 1) {
+        segonaCarta = e.textContent;
+        alert(`Has apretat la carta ${e.textContent}`);
         contador++;
-    } else if (contador == 2) {
+    } 
+    if (contador == 2) {
         if (primeraCarta == segonaCarta) {
             alert('son iguals')
+            contador = 0;
         } else {
-            alert('has perdut')
-        }
-    }
-}
+            alert('no son iguals');
+            contador = 0;
+        };
+    };
+};
